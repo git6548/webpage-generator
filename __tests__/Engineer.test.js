25fd3f3b02262833e78513ca725354a62db98e67
Stephanie <stephanieanderson5133@gmail.com>
2022-03-04 16:45:37 -0600

    tests and constructors done

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,6 +2,6 @@ const Engineer = require('../lib/Engineer');
 
 
 test('should create a class with a name, id, email, and github', () => {
-    const engineer = new Engineer();
-    expect(typeof engineer).toBe('object');
+    const engineer = new Engineer('Someone', 7, 'someone@something.com', 'someonesgithub');
+    expect(engineer.github).toEqual('someonesgithub');
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,8 +1,5 @@
 const Manager = require('../lib/Manager');
 
-
-//describe('Manager', () => {
-
 test('should create a class with a name, id, email, and an office number ', () => {
     const manager = new Manager ('Someone', 7, 'someone@something.com', 7);
     expect(manager.officeNumber).toEqual(7);
